14e52f0287f898527f82cc997af80e1687c93d42
Johannes Kastl <kastl@b1-systems.de>
2020-05-11 08:30:06 +0200

    ADD update_and_commit_git_module.sh: update a git submodule to the latest state on master, check out the latest commit and commit the changes (in the parent repo, not in the git submodule itself)

---
+++ update_and_commit_git_module.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+
+#
+# Pre-checks
+#
+
+[ "$#" == "1" ] || {
+        echo "Please give the module name or path as single argument. Aborting..."
+        exit 1
+}
+
+MODULE="${PWD}/${1}"
+echo "Working on git submodule ${MODULE}"
+
+[ -d "${MODULE}" ] || {
+        echo "Directory ${MODULE} does not exist. Aborting..."
+        exit 2
+}
+
+#
+# Check if either a 'meta/main.yml' file (for ansible roles) or a 'README.md' file (for terraform modules) is existing
+# Abort if neither exists.
+#
+{ 
+        [ -e "${MODULE}/meta/main.yml" ] || [ -e "${MODULE}/README.md" ]
+} || {
+        echo "git submodule ${MODULE} is not fully initalized and checked out."
+        echo "Run 'git submodule init ${MODULE} && git submodule update ${MODULE}'"
+        exit 2
+}
+
+#
+# Ready for take off...
+#
+
+pushd "${MODULE}" > /dev/null || exit 11
+
+if LANG=C git status | grep -q "nothing to commit, working tree clean"
+then
+        echo "Working directory is clean, continuing..."
+else
+        echo "Working directory is dirty, aborting..."
+        git status
+        exit 12
+fi
+
+git checkout master || exit 13
+
+git pull || exit 15
+
+HASH_OF_LAST_COMMIT="$(git rev-parse --short HEAD)"
+MESSAGE_OF_LAST_COMMIT="$(git log -1 --pretty=%B)"
+
+[ -z "${HASH_OF_LAST_COMMIT}" ] && {
+        echo "Variable HASH_OF_LAST_COMMIT is empty, aborting..."
+        exit 16
+}
+
+[ -z "${MESSAGE_OF_LAST_COMMIT}" ] && {
+        echo "Variable MESSAGE_OF_LAST_COMMIT  is empty, aborting..."
+        exit 17
+}
+
+git checkout "${HASH_OF_LAST_COMMIT}" || exit 18
+
+popd > /dev/null || exit 19
+
+git add "${MODULE}" || exit 21
+
+git commit -m "UPDATE git submodule ${MODULE} to commit ${HASH_OF_LAST_COMMIT}: ${MESSAGE_OF_LAST_COMMIT}" || exit 23
+
+exit 0
